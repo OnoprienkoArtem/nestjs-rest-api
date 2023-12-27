723da48e7847d7ee932a4daaaa263b1d9c294962
ArtemOnopriienko <icartem.onopriienko@americanwell.com>
2023-12-27 20:26:51 +0200

    small refactoring

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { CustomExceptionFilter } from './exception-filters/custom.exception-filt
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExceptionFilter());
   app.enableCors({
@@ -12,4 +13,5 @@ async function bootstrap() {
   });
   await app.listen(process.env.APP_PORT, '0.0.0.0');
 }
+
 bootstrap();
